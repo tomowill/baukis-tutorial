996692ab15fe2219977511a3d262f7dc5d8644d1
tomowill <tomo144@gmail.com>
2016-10-03 00:22:03 +0900

    advanced to 5-2

--- app/controllers/staff/programs_controller.rb
+++ app/controllers/staff/programs_controller.rb
@@ -14,4 +14,36 @@ class Staff::ProgramsController < Staff::Base
   def edit
     @program_form = Staff::ProgramForm.new(Program.find(params[:id]))
   end
+
+  def create
+    @program_form = Staff::ProgramForm.new
+    @program_form.assign_attributes(params[:form])
+    @program_form.program.registrant = current_staff_member
+    if @program_form.save
+      flash.notice = 'プログラムを登録しました。'
+      redirect_to action: 'index'
+    else
+      flash.now.alert = '入力に誤りがあります。'
+      render action: 'new'
+    end
+  end
+
+  def update
+    @program_form = Staff::ProgramForm.new(Program.find(params[:id]))
+    @program_form.assign_attributes(params[:form])
+    if @program_form.save
+      flash.notice = 'プログラムを更新しました。'
+      redirect_to action: 'index'
+    else
+      flash.now.alert = '入力に誤りがあります。'
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    program = Program.find(params[:id])
+    program.destroy!
+    flash.notice = 'プログラムを削除しました。'
+    redirect_to :staff_programs
+  end
 end
--- app/presenters/program_form_presenter.rb
+++ app/presenters/program_form_presenter.rb
@@ -4,12 +4,18 @@ class ProgramFormPresenter < FormPresenter
       m << decorated_label(:description, '詳細', required: true)
       m << text_area(:description, rows: 6, style: 'width: 454px')
       m.span '(800文字以内)', class: 'instruction', style: 'float: right'
+      m << error_messages_for(:description)
     end
   end
 
   def datetime_field_block(name, label_text, options = {})
     instruction = options.delete(:instruction)
-    markup(:div, class: 'input-block') do |m|
+    if object.errors.include?("#{name}_time".to_sym)
+      html_class = 'input-block with-errors'
+    else
+      html_class = 'input-block'
+    end
+    markup(:div, class: 'html_class') do |m|
       m << decorated_label("#{name}_date", label_text, options)
       if options[:class].kind_of?(String)
         classes = options[:class].strip.split + ['datepicker']
@@ -21,7 +27,9 @@ class ProgramFormPresenter < FormPresenter
       m << form_builder.select("#{name}_hour", ('00'..'23').to_a)
       m << ':'
       m << form_builder.select("#{name}_minute", ('00'..'59').to_a)
-      m .span "(#{instruction})", class: 'instruction' if instruction
+      m.span "(#{instruction})", class: 'instruction' if instruction
+      m << error_messages_for("#{name}_time".to_sym)
+      m << error_messages_for("#{name}_date".to_sym)
     end
   end
 end
