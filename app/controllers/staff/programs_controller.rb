6a3bec4f5a0ac0ad41d46d2673789774bd70ab5f
tomowill <tomo144@gmail.com>
2016-10-03 13:24:49 +0900

    merge

--- app/controllers/staff/programs_controller.rb
+++ app/controllers/staff/programs_controller.rb
@@ -1,10 +1,49 @@
 class Staff::ProgramsController < Staff::Base
   def index
-    @programs = Program.order(application_start_time: :desc)
-      .page(params[:page])
+    @programs = Program.listing.page(params[:page])
   end
 
   def show
-    @program = Program.find(params[:id])
+    @program = Program.listing.find(params[:id])
+  end
+
+  def new
+    @program_form = Staff::ProgramForm.new
+  end
+
+  def edit
+    @program_form = Staff::ProgramForm.new(Program.find(params[:id]))
+  end
+
+  def create
+    @program_form = Staff::ProgramForm.new
+    @program_form.assign_attributes(params[:form])
+    @program_form.program.registrant = current_staff_member
+    if @program_form.save
+      flash.notice = 'プログラムを登録しました。'
+      redirect_to action: 'index'
+    else
+      flash.now.alert = '入力に誤りがあります。'
+      render action: 'new'
+    end
+  end
+
+  def update
+    @program_form = Staff::ProgramForm.new(Program.find(params[:id]))
+    @program_form.assign_attributes(params[:form])
+    if @program_form.save
+      flash.notice = 'プログラムを更新しました。'
+      redirect_to action: 'index'
+    else
+      flash.now.alert = '入力に誤りがあります。'
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    program = Program.find(params[:id])
+    program.destroy!
+    flash.notice = 'プログラムを削除しました。'
+    redirect_to :staff_programs
   end
 end
